fc1ba8df8bffee374b63b6b7c019d270931cd345
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-10-25 22:18:46 +0530

    feat: restructure server files and add mongoose for database connection
    - Updated package.json to change the entry point to src/app.js and added mongoose as a dependency.
    - Created src/app.js to set up the Express server and middleware.
    - Added src/config/connectDB.js for future database connection logic.

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,16 @@
 // External Modules
-const express = require('express');
+const express = require("express");
 require("@dotenvx/dotenvx").config(); // with dotenvx now we can encrypt .env files
 
 // Local Modules
 
-
 const app = express();
-
 
 //middleware
 app.use(express.json());
-
-
 
 // Server Start
 const PORT = process.env.PORT;
 app.listen(PORT || 1000, () => {
     console.log(`server is running...`);
-})
+});
---
+++ server/src/config/connectDB.js
@@ -0,0 +1,5 @@
+const {connect } = require('mongoose');
+
+const connectDB = async () => {
+    
+};
