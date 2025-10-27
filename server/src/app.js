507157a08027a15b17ce35c35d23959248d15b5f
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-10-27 22:17:18 +0530

    feat: update database connection configuration and improve connection handling

--- server/src/app.js
+++ server/src/app.js
@@ -3,14 +3,16 @@ const express = require("express");
 require("@dotenvx/dotenvx").config(); // with dotenvx now we can encrypt .env files
 
 // Local Modules
+require("./config/connectDB");
 
 const app = express();
 
 //middleware
 app.use(express.json());
 
+
 // Server Start
 const PORT = process.env.PORT;
 app.listen(PORT || 1000, () => {
-    console.log(`server is running...`);
+	console.log(`server is running...`);
 });
--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -1,5 +1,16 @@
-const {connect } = require('mongoose');
+const { connect } = require("mongoose");
+require("@dotenvx/dotenvx").config();
+
+const MONGO_DB_URI = process.env.MONGODB_URL || "";
+const DB_NAME = process.env.MONGODB_DB_NAME || "";
 
 const connectDB = async () => {
-    
+	try {
+		await connect(`${MONGO_DB_URI}/${DB_NAME}`,);
+		console.log("MongoDB connection successful");
+	} catch (error) {
+		console.error(error);
+	}
 };
+
+connectDB();
